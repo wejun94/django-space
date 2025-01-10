3050d3bafdf891a26ef13faf5a4a747782d11931
Wellington Júnior <prowebstruct@gmail.com>
2025-01-10 18:01:31 -0500

    nl3 aula 06

--- ALURA-SPACE/galeria/models.py
+++ ALURA-SPACE/galeria/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# classe que é igual a um model, pois ela representa uma tabela no banco de dados
+class Fotografia(models.Model):
+    #colunas (nome, legenda, descrição, foto)
+    #Parametro max_length=100, indica o máximo de caracteres que teremos dentro de nome
+    #null == false, ou seja, nao pode ser um campo vazio
+    #blank==False, ou seja, não pode ser um campo blank, desta forma -> ''
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(max_length=150, null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+    
+    def _str_(self):
+        return f"Fotografia [nome={self.nome}]"
+    
