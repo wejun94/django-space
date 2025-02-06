636f2ebdbe9bde301e72069b8f35299d3af0203c
Prows-developmente <prowebstruct@gmail.com>
2025-02-05 20:37:13 -0600

    django - persistencia - final

--- ALURA-SPACE/galeria/urls.py
+++ ALURA-SPACE/galeria/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, buscar
 
 urlpatterns = [
     path('', index, name="index"),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path("buscar", buscar, name="buscar"),#nome do path | aponta para a rota no views | name buscar
+    
 ]
