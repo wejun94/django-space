0bd260d693f9e39cda1a36608360c2881e413c56
Prows-developmente <prowebstruct@gmail.com>
2025-02-25 13:51:50 -0600

    forms.py created

--- ALURA-SPACE/usuarios/forms.py
+++ ALURA-SPACE/usuarios/forms.py
@@ -2,24 +2,70 @@ from django import forms
 
 class LoginForms(forms.Form):
     nome_login=forms.CharField(
-        label='Nome de Login',
-        required=True,
+        label='Nome de Login', 
+        required=True, 
         max_length=100,
         widget=forms.TextInput(
             attrs={
-                "class": "form-control"
-                "placeholder": "Ex Wellington Junior"
+                'class': 'form-control',
+                'placeholder': 'Ex.: João Silva',
             }
         )
     )
     senha=forms.CharField(
-        label="Senha",
-        required=True,
+        label='Senha', 
+        required=True, 
         max_length=70,
         widget=forms.PasswordInput(
             attrs={
-                "class": "form-control" #estas formatações tem como objetivo de estilizar as caixas de entrada no formulário
-                "placeholder": "Sua senha de 7 caracteres ou mais"
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha',
+            }
+        ),
+    )
+
+class CadastroForms(forms.Form):
+    nome_cadastro=forms.CharField(
+        label='Nome de Cadastro', 
+        required=True, 
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: João Silva',
             }
         )
+    )
+    email=forms.EmailField(
+        label='Email',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: joaosilva@xpto.com',
+            }
+        )
+    )
+    senha_1=forms.CharField(
+        label='Senha', 
+        required=True, 
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha',
+            }
+        ),
+    )
+    senha_2=forms.CharField(
+        label='Confirme a sua senha', 
+        required=True, 
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha novamente',
+            }
+        ),
     )
