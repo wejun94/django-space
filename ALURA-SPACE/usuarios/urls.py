ad2c7bda3ff4cd790279922e0dfff90c8bfa69f8
Prows-developmente <prowebstruct@gmail.com>
2025-03-03 15:35:50 -0600

    cadastro, login, alertas

--- ALURA-SPACE/usuarios/urls.py
+++ ALURA-SPACE/usuarios/urls.py
@@ -1,9 +1,10 @@
 # usuarios > urls.py
 
 from django.urls import path
-from usuarios.views import login, cadastro
+from usuarios.views import login, cadastro, logout
 
 urlpatterns = [
     path('login', login, name='login'),
     path('cadastro', cadastro, name='cadastro'),
+    path('logout', logout, name='logout'),
 ]
--- ALURA-SPACE/usuarios/views.py
+++ ALURA-SPACE/usuarios/views.py
@@ -4,8 +4,34 @@ from usuarios.forms import LoginForms, CadastroForms
 
 from django.contrib.auth.models import User
 
+from django.contrib import auth # Necessário para usar o método authenticate()
+
+from django.contrib import messages
+
 def login(request):
      form = LoginForms()
+     
+     if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():# Se os dados são válidos, tenta autenticar o usuário.
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+             
+        usuario = auth.authenticate(#
+            request,
+            username=nome, 
+            password=senha
+        )
+        # Se o nome de usuário e a senha estão corretos, autentica o usuário e redireciona para a página de index.
+        if usuario is not None:
+            auth.login(request, usuario)
+            messages.success(request, f"{nome}Usuário logado com sucesso!")
+            return redirect('index')
+        else:
+            messages.error(request, "Erro ao efectuar o login!")
+            return redirect('login')
+     
      return render(request, 'usuarios/login.html', {'form': form})
 
 def cadastro(request):
@@ -18,6 +44,7 @@ def cadastro(request):
 
         if form.is_valid(): # Se os dados são válidos, cria um novo usuário e redireciona para a página de login.
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, "Senhas não coincidem")
                 return redirect ('cadastro')
 
             nome=form['nome_cadastro'].value()
@@ -25,6 +52,7 @@ def cadastro(request):
             senha=form['senha_1'].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, "Nome de usuário já existe")
                 return redirect('cadastro')# Se o nome de usuário já existe, redireciona para a página de cadastro.
 
             usuario = User.objects.create_user(# Cria um novo usuário
@@ -33,6 +61,11 @@ def cadastro(request):
                 password=senha
             )
             usuario.save()# Salva o novo usuário no banco de dados e redireciona para a página de login.
+            messages.sucess(request, f"{nome} Usuário criado com sucesso")
             return redirect('login')
-
     return render(request, 'usuarios/cadastro.html', {'form': form})
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "Usuário deslogado com Sucesso")
+    return redirect('login')
